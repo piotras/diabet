21016ea7c8cb4f002f0ec452bc7b6ede461e96d5
piotras <piotrek.pokora@gmail.com>
2015-10-16 10:31:51 +0200

    Default empty dict in constructor. Set and get default basal rate.

--- diabet/person.py
+++ diabet/person.py
@@ -2,8 +2,12 @@
 from errors import ValueException
 
 class Person(object):
-    def __init__(self, data=None):
+    def __init__(self, data={}):
         self.data = data
+
+    def _validate_key_in_data(self, key, error):
+        if key not in self.data:
+            raise ValueException(error)
 
     def get_name(self):
         """
@@ -12,6 +16,30 @@ class Person(object):
         :returns: name of the person
         :raises: ValueException on error
         """
-        if self.data is None or 'name' not in self.data:
-            raise ValueException("Missing name of the person")
+        self._validate_key_in_data('name', 'Missing name of the person')
         return self.data['name']
+
+    def set_default_basal_rate(self, rate):
+        """
+        Sets default basal rate.
+
+        Basal Rate: The rate at which an insulin pump infuses small, “background” doses of short-acting insulin.
+        http://www.diabetesselfmanagement.com/diabetes-resources/definitions/basal-rate/
+
+        Basal rate 0 is allowed because person might not have insulin pump.
+        With traditional injections, basal rate can not be set.
+
+        :raises: ValueException in case of invalid rate.
+        """
+        if rate < 0:
+            raise ValueException('Invalid, negative value of basal rate')
+        self.data['default_basal_rate'] = rate
+
+    def get_default_basal_rate(self):
+        """
+        Get default basal rate.
+
+        :raises: ValueException in case of invalid value.
+        """
+        self._validate_key_in_data('default_basal_rate', 'Missing  default_basal_rate')
+        return self.data['default_basal_rate']
